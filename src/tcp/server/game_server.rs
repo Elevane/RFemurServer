230aeb064625e9f60d130eb128d101a12df63c61
Bastien AUBRY <pma.bastien.aubry@gmail.com>
2025-03-14 23:09:31 +0100

    gg

--- src/tcp/server/game_server.rs
+++ src/tcp/server/game_server.rs
@@ -1,44 +1,50 @@
-use std::fmt::format;
+
 
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
 pub struct Server {
-    max_allowed_connections: i16,
+    max_allowed_connections:i16,
     port: i16,
-    is_running: bool,
+    connections: Vec<Connection>
 }
 
 impl Server {
-    pub fn new(max_allowed_connections: Option<i16>, port: Option<i16>) -> Self {
+    pub fn new(&self, max_allowed_connections: Option<i16>, port: Option<i16>) -> Self {
         Self {
-            max_allowed_connections: max_allowed_connections.unwrap_or(5000),
+            max_allowed_connections : max_allowed_connections.unwrap_or(50000),
             port: port.unwrap_or(3333),
-            is_running: true,
+            connections: Vec::new(),
         }
     }
 
     pub async fn start(&self) -> Result<(), Box<dyn std::error::Error>> {
         let address = format!("{}:{}", "127.0.0.1", self.port);
-        print!("Starting server on {}", address);
+        println!("Starting server on {}", address);
         let listener = TcpListener::bind(address).await.unwrap();
 
         loop {
             // The second item contains the IP and port of the new connection.
             let (socket, _) = listener.accept().await.unwrap();
+            
             self.handle_client(socket).await;
         }
     }
 
     async fn handle_client(&self, socket: TcpStream) {
-        // The `Connection` lets us read/write redis **frames** instead of
-        // byte streams. The `Connection` type is defined by mini-redis.
         let mut connection = Connection::new(socket);
-
+        let size = {
+            let locked_vec = self.connections.to_vec().lock().await;
+            locked_vec.len()
+        };
+        if(size >  self.max_allowed_connections)
+        {
+            
+            connection.write_frame("Too much connexion atm");
+            socket.shutdown();
+        }
         if let Some(frame) = connection.read_frame().await.unwrap() {
             println!("GOT: {:?}", frame);
-
-            // Respond with an error
             let response = Frame::Error("unimplemented".to_string());
             connection.write_frame(&response).await.unwrap();
         }
